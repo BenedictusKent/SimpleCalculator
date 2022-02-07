5058bfae8ecaf4c31ad9c4ca60fa830490aa07a2
Benedictus Kent <benedictuskent123@gmail.com>
2022-02-08 00:33:26 +0800

    remove and add variables

--- simplecalculator/lib/main.dart
+++ simplecalculator/lib/main.dart
@@ -26,10 +26,8 @@ class MyApp extends StatelessWidget {
 }
 
 class CalcApp extends ConsumerWidget {
-  static var arrayNum = [];
-  static var arrayOps = [];
-  static int firstNum = 0;
   static int operator = 0;
+  static int reset = 0;
   final int colorBlack = 0xFF000000;
   final int colorGrey = 0xFFE0E0E0;
   final int colorOrange = 0xFFFFA726;
